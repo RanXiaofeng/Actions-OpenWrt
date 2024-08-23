40455f6dd41f19b00795c00c4c5318fa18bde4ca
RanGaofeng <174862821+RanXiaofeng@users.noreply.github.com>
2024-08-24 01:16:27 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,6 +31,9 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 #sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
 sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci/Makefile
 
+#OpenWrt-mihomo
+git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default
+
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
 
 #ddns-go
